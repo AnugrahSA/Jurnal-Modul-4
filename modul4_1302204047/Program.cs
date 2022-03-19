3accf5f56946dc1ab811dd762e758be7361843e7
anugrahSA <anugrahnugi321@gmail.com>
2022-03-19 15:18:51 +0800

    menambahkan class SimpleDateBase

--- modul4_1302204047/Program.cs
+++ modul4_1302204047/Program.cs
@@ -10,6 +10,11 @@ namespace modul4_1302204047
         {
 
             Console.WriteLine(Penjumlahan.JumlahTigaAngka<int>(13, 02, 20));
+            SimpleDataBase<int> data = new SimpleDataBase<int>();
+            data.AddNewData(12);
+            data.AddNewData(34);
+            data.AddNewData(56);
+            data.PrintAllData();
         }
     }
     class Penjumlahan
